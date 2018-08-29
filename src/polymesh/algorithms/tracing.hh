4969a0c9861f309424f3c66d31910c6a818fb0fe
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2018-08-29 16:01:44 +0200

    working on tracing

---
+++ src/polymesh/algorithms/tracing.hh
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../Mesh.hh"
+#include "../fields.hh"
+
+namespace polymesh
+{
+
+/**
+ * Traces a ray inside the triangle stopping at the next edge
+ */
+
+/// Traces from halfedge h into its triangle
+/// Start point is given by interpolating h.from and h.to given x
+/// Direction is given by virtual source with SQUARED distance d1_sqr from h.from and d2_sqr from h.to
+/// Returns intersecting half-edge and new x parameter
+template <class Scalar = float, class EdgeLengthF>
+std::pair<halfedge_handle, float> trace_step(halfedge_handle h, EdgeLengthF&& edge_length, Scalar x, Scalar d1_sqr, Scalar d2_sqr);
+
+/// ======== IMPLEMENTATION ========
+
+template <class Scalar, class EdgeLengthF>
+std::pair<halfedge_handle, float> trace_step(halfedge_handle h, EdgeLengthF&& edge_length, Scalar x, Scalar d1_sqr, Scalar d2_sqr)
+{
+    assert(!h.is_boundary() && "cannot trace into boundary");
+    auto f = h.face();
+    assert(f.vertices().size() == 3 && "only supports triangles");
+
+    // TODO
+}
+
+}
