516b04388261f705f9a9903af6fd68067debceae
Philip Trettner <philip.trettner@rwth-aachen.de>
2019-04-09 15:36:26 +0200

    improved unused macro again

--- src/polymesh/macros.hh
+++ src/polymesh/macros.hh
@@ -55,4 +55,8 @@
 #define POLYMESH_DETAIL_MACRO_JOIN(arg1, arg2) arg1##arg2
 #define POLYMESH_MACRO_JOIN(arg1, arg2) POLYMESH_DETAIL_MACRO_JOIN(arg1, arg2)
 
-#define POLYMESH_UNUSED(expr) (void)(decltype(expr)) false // force ;
+#define POLYMESH_UNUSED(expr) \
+    do                        \
+    {                         \
+        void(sizeof(bool((expr))));   \
+    } while (false) // force ;
