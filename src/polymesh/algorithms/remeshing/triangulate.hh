3f26d70bd0d9121e5229258e8ae3df2453da185c
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2019-06-12 17:46:20 +0200

    Added delabella library for 2d triangulation.
    Signed-off-by: Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>

--- src/polymesh/algorithms/remeshing/triangulate.hh
+++ src/polymesh/algorithms/remeshing/triangulate.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <polymesh/Mesh.hh>
+#include <polymesh/detail/delaunay.hh>
+#include <unordered_set>
 
 // Basic mesh operations, including:
 // - elementary subdivision
@@ -10,6 +12,10 @@ namespace polymesh
 {
 /// Given a flat polymesh with convex faces, naively triangulates all faces
 void triangulate_naive(Mesh& m);
+
+/// Given a 2d mesh filled with vertices, creates a delauny triangulation
+template <class Vec2>
+bool add_delauny_triangulation(Mesh& m, vertex_attribute<Vec2> const& position);
 
 /// ======== IMPLEMENTATION ========
 
@@ -32,4 +38,21 @@ inline void triangulate_naive(Mesh& m)
             m.faces().add(vs[0], vs[i - 1], vs[i]);
     }
 }
+
+template <class Pos2>
+bool add_delauny_triangulation(Mesh& m, vertex_attribute<Pos2> const& pos)
+{
+    POLYMESH_ASSERT(m.vertices().size() >= 3 && "Mesh must have at least 3 vertices");
+
+    auto p = std::vector<float>(pos.size() * 2);
+    for (auto i = 0u; i < pos.size(); ++i)
+    {
+        p[i * 2 + 0] = float(pos[vertex_index(i)][0]);
+        p[i * 2 + 1] = float(pos[vertex_index(i)][1]);
+    }
+
+    return detail::add_delauny_triangulation_delabella(m, p.data());
 }
+
+
+} // namespace polymesh
---
+++ src/polymesh/detail/delaunay.hh
@@ -0,0 +1,9 @@
+#pragma once
+#include "../Mesh.hh"
+namespace polymesh
+{
+namespace detail
+{
+bool add_delauny_triangulation_delabella(Mesh &m, float const *pos);
+}
+} // namespace polymesh
