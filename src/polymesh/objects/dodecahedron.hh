57b573b6863c7497b3c700de032fb3d73f147697
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2020-10-12 14:08:58 +0200

    Added dodecahedron object

---
+++ src/polymesh/objects/dodecahedron.hh
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <polymesh/Mesh.hh>
+#include <polymesh/detail/math.hh>
+#include <polymesh/detail/unique_array.hh>
+#include <polymesh/fields.hh>
+
+namespace polymesh::detail
+{
+unique_array<pos3f> add_unit_dodecahedron_impl(Mesh& m);
+}
+
+namespace polymesh::objects
+{
+/// Adds a (subdivided) ico_sphere to the given mesh
+/// sf is called with (v, x, y, z), with vertex handle v and coordinates (x,y,z) on the unit sphere
+template <class SphereF>
+vertex_handle add_dodecahedron(Mesh& m, SphereF&& sf);
+
+/// same as add_cube but directly fills a position attribute
+template <class Pos3>
+vertex_handle add_dodecahedron(Mesh& m, vertex_attribute<Pos3>& pos);
+
+// ======== IMPLEMENTATION ========
+
+template <class SphereF>
+vertex_handle add_dodecahedron(Mesh& m, SphereF&& sf)
+{
+    auto const last_vertex_count = m.all_vertices().size();
+    auto const positions = detail::add_unit_dodecahedron_impl(m);
+    for (auto i = 0; i < 20; ++i)
+    {
+        sf(vertex_index(last_vertex_count + i).of(m), positions[i].x, positions[i].y, positions[i].z);
+    }
+    return vertex_index(last_vertex_count).of(m);
+}
+
+template <class Pos3>
+auto add_dodecahedron(Mesh& m, vertex_attribute<Pos3>& pos) -> vertex_handle
+{
+    return add_dodecahedron(m, [&](vertex_handle v, float x, float y, float z) { pos[v] = field3<Pos3>::make_pos(x, y, z); });
+}
+}
