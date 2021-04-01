76e20b6b8aa4b80cca568922dc5907a49c2136b0
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2021-04-01 13:04:17 +0200

    improved hash for handles

--- src/polymesh/hash.hh
+++ src/polymesh/hash.hh
@@ -22,11 +22,11 @@ struct hash
     template <class tag>
     size_t operator()(polymesh::primitive_handle<tag> const& i) const noexcept
     {
-        return i.idx.value;
+        return size_t(i.idx.value) ^ size_t(i.mesh);
     }
-    size_t operator()(polymesh::face_handle const& i) const noexcept { return i.idx.value; }
-    size_t operator()(polymesh::vertex_handle const& i) const noexcept { return i.idx.value; }
-    size_t operator()(polymesh::edge_handle const& i) const noexcept { return i.idx.value; }
-    size_t operator()(polymesh::halfedge_handle const& i) const noexcept { return i.idx.value; }
+    size_t operator()(polymesh::face_handle const& i) const noexcept { return size_t(i.idx.value) ^ size_t(i.mesh); }
+    size_t operator()(polymesh::vertex_handle const& i) const noexcept { return size_t(i.idx.value) ^ size_t(i.mesh); }
+    size_t operator()(polymesh::edge_handle const& i) const noexcept { return size_t(i.idx.value) ^ size_t(i.mesh); }
+    size_t operator()(polymesh::halfedge_handle const& i) const noexcept { return size_t(i.idx.value) ^ size_t(i.mesh); }
 };
 }
